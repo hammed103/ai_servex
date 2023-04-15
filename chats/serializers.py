f9ee5ee0e7b1686f86a3c63fab93c1a4510adb93
hammed103 <hammedfree@gmail.com>
2023-04-15 14:45:59 +0100

    profiles

--- chats/serializers.py
+++ chats/serializers.py
@@ -2,20 +2,26 @@ from rest_framework import serializers
 
 from .models import Chat, Message
 
+
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
-        fields = '__all__'
+        fields = "__all__"
+
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ChatMessageSerializer(serializers.Serializer):
     chat_id = serializers.UUIDField(required=False)
     message = serializers.CharField(required=False)
+    latitude = serializers.DecimalField(max_digits=9, decimal_places=6)
+    longitude = serializers.DecimalField(max_digits=9, decimal_places=6)
+    address = serializers.CharField(required=False)
 
 
 class ChatResponseSerializer(serializers.Serializer):
-    message = serializers.CharField()
+    message = serializers.DictField()
--- chats/views.py
+++ chats/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import openai
 import re
+import requests
 
 
 from .models import Chat, Message
@@ -13,7 +14,69 @@ from .serializers import (
     ChatSerializer,
 )
 
-openai.api_key = "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"
+
+def more(pid):
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={pid}&key={api_key}"
+
+    response = requests.request(
+        "GET",
+        url,
+    ).json()
+    try:
+        reviews = response["result"]["reviews"]
+    except:
+        reviews = {}
+
+    try:
+        website = response["result"]["website"]
+    except:
+        website = ""
+
+    try:
+        user_ratings_total = response["result"]["user_ratings_total"]
+    except:
+        user_ratings_total = 0
+
+    try:
+        PHOTO_REFERENCE = (
+            "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference="
+            + response["result"]["photos"][0]
+            + api_key
+        )
+
+    except:
+        PHOTO_REFERENCE = ""
+
+    return {
+        "website": website,
+        "user_ratings_total": user_ratings_total,
+        "reviews": reviews,
+        "PHOTO_REFERENCE": PHOTO_REFERENCE,
+    }
+
+
+import math
+
+
+def haversine_distance(coord1, coord2):
+    # Convert coordinates to radians
+    lat1, lon1 = math.radians(coord1["lat"]), math.radians(coord1["lng"])
+    lat2, lon2 = math.radians(coord2[0]), math.radians(coord2[1])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = (
+        math.sin(dlat / 2) ** 2
+        + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    )
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = 6371 * c  # Radius of Earth in kilometers
+
+    return distance
+
+
+openai.api_key = "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"
 
 
 class ChatGPT(APIView):
@@ -22,6 +85,9 @@ class ChatGPT(APIView):
         if serializer.is_valid():
             request_message = serializer.validated_data.get("message", None)
             chat_id = serializer.validated_data.get("chat_id", None)
+            address = serializer.validated_data.get("address", None)
+            latitude = serializer.validated_data.get("latitude", None)
+            longitude = serializer.validated_data.get("longitude", None)
 
             if chat_id:
                 chat = Chat.objects.get(id=chat_id)
@@ -81,14 +147,98 @@ class ChatGPT(APIView):
             matches = [i.split(" - ")[0] for i in matches]
 
             if len(matches) > 2:
-                ai_message = "\n".join(matches)
+                api_key = "AIzaSyDmWkVPiZKZoGSI4NHfL-GBhvOFPOFtRiY"
+
+                def more(pid):
+                    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={pid}&key={api_key}"
+
+                    response = requests.request(
+                        "GET",
+                        url,
+                    ).json()
+                    try:
+                        reviews = response["result"]["reviews"]
+                    except:
+                        reviews = {}
+
+                    try:
+                        website = response["result"]["website"]
+                    except:
+                        website = ""
+
+                    try:
+                        user_ratings_total = response["result"]["user_ratings_total"]
+                    except:
+                        user_ratings_total = 0
+
+                    try:
+                        PHOTO_REFERENCE = (
+                            "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference="
+                            + response["result"]["photos"][0]
+                            + api_key
+                        )
+
+                    except:
+                        PHOTO_REFERENCE = ""
+
+                    return {
+                        "website": website,
+                        "user_ratings_total": user_ratings_total,
+                        "reviews": reviews,
+                        "PHOTO_REFERENCE": PHOTO_REFERENCE,
+                    }
+
+                address_search = matches[0] + " around " + address
+
+                url = (
+                    "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
+                    + address_search
+                    + f"&key={api_key}"
+                )
+
+                payload = {}
+                headers = {}
+
+                response = requests.request("GET", url, headers=headers, data=payload)
+                lantern = [
+                    {
+                        "business_name": i["name"],
+                        "address": i["formatted_address"],
+                        "specialty": i["types"][0],
+                        "place_id": i["place_id"],
+                        "rating": i["rating"],
+                        "distance": round(
+                            haversine_distance(
+                                i["geometry"]["location"], (latitude, longitude)
+                            ),
+                            2,
+                        ),
+                    }
+                    for i in response.json()["results"]
+                ]
+                # Key to be updated and its corresponding function
+                key_to_update = "place_id"
+                function_to_apply = more
+
+                # List comprehension to add more keys by applying a function
+                # updated_list_of_dicts = [{**d, 'updated_key': function_to_apply(d[key_to_update],api_key)} for d in lantern]
+                updated_list_of_dicts = [
+                    {**d, **function_to_apply(d[key_to_update])} for d in lantern
+                ]
+
+                updated_list_of_dicts = [
+                    i for i in updated_list_of_dicts if len(i["reviews"]) > 0
+                ]
+                ai_messager = {"result": updated_list_of_dicts}
+            else:
+                ai_messager = {"result": ai_message}
 
             ai_message_obj = Message(
                 content=ai_message, role=Message.RoleChoices.ASSISTANT, chat=chat
             )
             ai_message_obj.save()
 
-            response_serializer = ChatResponseSerializer(data={"message": ai_message})
+            response_serializer = ChatResponseSerializer(data={"message": ai_messager})
             if response_serializer.is_valid():
                 return Response(
                     {
